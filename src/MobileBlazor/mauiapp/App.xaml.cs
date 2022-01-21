0625ea9cbdb8a25e77aea1556ef327ef08206540
James Montemagno <james.montemagno@gmail.com>
2022-01-21 15:25:32 -0800

    Update maui blazor app

--- src/MobileBlazor/mauiapp/App.xaml.cs
+++ src/MobileBlazor/mauiapp/App.xaml.cs
@@ -1,17 +1,13 @@
-﻿using Microsoft.Maui;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
-using Application = Microsoft.Maui.Controls.Application;
+﻿using Application = Microsoft.Maui.Controls.Application;
 
-namespace NetPodsMauiBlazor
+namespace NetPodsMauiBlazor;
+
+public partial class App : Application
 {
-    public partial class App : Application
+    public App()
     {
-        public App()
-        {
-            InitializeComponent();
+        InitializeComponent();
 
-            MainPage = new MainPage();
-        }
+        MainPage = new MainPage();
     }
 }
