0cf745e6d2beb14fc8feb75393098fe3a0614f9e
James Montemagno <james.montemagno@gmail.com>
2022-01-24 10:51:09 -0800

    Use transient

--- src/Mobile/ViewModels/ViewModelExtensions.cs
+++ src/Mobile/ViewModels/ViewModelExtensions.cs
@@ -5,14 +5,14 @@ public static class ViewModelExtensions
     public static MauiAppBuilder ConfigureViewModels(this MauiAppBuilder builder)
     {
         builder.Services.AddSingleton<CategoriesViewModel>();
-        builder.Services.AddSingleton<CategoryViewModel>();
+        builder.Services.AddTransient<CategoryViewModel>();
         builder.Services.AddSingleton<DiscoverViewModel>();
-        builder.Services.AddSingleton<EpisodeDetailViewModel>();
+        builder.Services.AddTransient<EpisodeDetailViewModel>();
         builder.Services.AddSingleton<EpisodeViewModel>();
         builder.Services.AddSingleton<ListenLaterViewModel>();
         builder.Services.AddSingleton<SettingsViewModel>();
         builder.Services.AddSingleton<ShellViewModel>();
-        builder.Services.AddSingleton<ShowDetailViewModel>();
+        builder.Services.AddTransient<ShowDetailViewModel>();
         builder.Services.AddSingleton<ShowViewModel>();
         builder.Services.AddSingleton<SubscriptionsViewModel>();
 
