4939e05e816a15b3e0a1172335c8227e03a9933e
Brian Runck <brian.c.runck@gmail.com>
2022-03-07 17:24:06 -0500

    Support Shell DI changes in .NET MAUI P14

---
+++ src/Mobile/Pages/PagesExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Microsoft.NetConf2021.Maui.Pages
+{
+    public static class PagesExtensions
+    {
+        public static MauiAppBuilder ConfigurePages(this MauiAppBuilder builder)
+        {
+            builder.Services.AddSingleton<DiscoverPage>();
+            builder.Services.AddSingleton<SubscriptionsPage>();
+            builder.Services.AddSingleton<ListenLaterPage>();
+            builder.Services.AddSingleton<ListenTogetherPage>();
+            builder.Services.AddSingleton<SettingsPage>();
+
+            return builder;
+        }
+    }
+}
