180c26c572bbc42adab794f51e511fbc10c2f40a
Parker Bibus <parkerbibus@microsoft.com>
2022-03-22 11:09:48 -0700

    Second set of updates to test.

--- src/Mobile/Platforms/Android/Services/MediaPlayerService.cs
+++ src/Mobile/Platforms/Android/Services/MediaPlayerService.cs
@@ -302,7 +302,7 @@ public class MediaPlayerService : Service,
     {
         try
         {
-            if (NativeVersion.IsAtLeast(21))
+            if (PlatformVersion.IsAtLeast(21))
             {
                 MediaMetadataRetriever metaRetriever = new MediaMetadataRetriever();
 
