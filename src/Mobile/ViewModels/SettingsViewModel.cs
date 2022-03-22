eb144448912e7d068c9ec259eb0627c391a12e98
Parker Bibus <parkerbibus@microsoft.com>
2022-03-22 09:59:29 -0700

    Rename OSAppTheme to AppTheme.

--- src/Mobile/ViewModels/SettingsViewModel.cs
+++ src/Mobile/ViewModels/SettingsViewModel.cs
@@ -33,15 +33,15 @@ public class SettingsViewModel : BaseViewModel
 
     public SettingsViewModel()
     {
-        isDarkModeEnabled = Settings.Theme == OSAppTheme.Dark;
+        isDarkModeEnabled = Settings.Theme == AppTheme.Dark;
         isWifiOnlyEnabled = Settings.IsWifiOnlyEnabled;
     }
 
     private void ChangeUserAppTheme(bool activateDarkMode)
     {
         Settings.Theme = activateDarkMode 
-            ? OSAppTheme.Dark
-            : OSAppTheme.Light;
+            ? AppTheme.Dark
+            : AppTheme.Light;
 
         TheTheme.SetTheme();
     }
