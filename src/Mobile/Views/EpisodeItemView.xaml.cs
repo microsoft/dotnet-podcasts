7af42c8f5139994b6e27a3db2879b3c666532594
Miguel Ángel Barrera <mbarrera@plainconcepts.com>
2022-04-25 11:08:22 +0200

    wip

--- src/Mobile/Views/EpisodeItemView.xaml.cs
+++ src/Mobile/Views/EpisodeItemView.xaml.cs
@@ -1,43 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Microsoft.NetConf2021.Maui.Views;
 
-
-namespace Microsoft.NetConf2021.Maui.Views
+public partial class EpisodeItemView 
 {
-    public partial class EpisodeItemView 
-    {
-        public static readonly BindableProperty RemoveListenLaterCommandProperty =
+    public static readonly BindableProperty RemoveListenLaterCommandProperty =
+    BindableProperty.Create(
+        nameof(RemoveListenLaterCommand),
+        typeof(ICommand),
+        typeof(EpisodeItemView),
+        default(string));
+
+    public static readonly BindableProperty RemoveListenLaterCommandParameterProperty =
         BindableProperty.Create(
-            nameof(RemoveListenLaterCommand),
-            typeof(ICommand),
-            typeof(EpisodeItemView),
-            default(string));
+            nameof(RemoveListenLaterCommandParameter),
+            typeof(EpisodeViewModel),
+            typeof(ShowItemView),
+            default(EpisodeViewModel));
 
-        public static readonly BindableProperty RemoveListenLaterCommandParameterProperty =
-            BindableProperty.Create(
-                nameof(RemoveListenLaterCommandParameter),
-                typeof(EpisodeViewModel),
-                typeof(ShowItemView),
-                default(EpisodeViewModel));
+    public ICommand RemoveListenLaterCommand
+    {
+        get { return (ICommand)GetValue(RemoveListenLaterCommandProperty); }
+        set { SetValue(RemoveListenLaterCommandProperty, value); }
+    }
 
-        public ICommand RemoveListenLaterCommand
-        {
-            get { return (ICommand)GetValue(RemoveListenLaterCommandProperty); }
-            set { SetValue(RemoveListenLaterCommandProperty, value); }
-        }
+    public EpisodeViewModel RemoveListenLaterCommandParameter
+    {
+        get { return (EpisodeViewModel)GetValue(RemoveListenLaterCommandParameterProperty); }
+        set { SetValue(RemoveListenLaterCommandParameterProperty, value); }
+    }
 
-        public ShowViewModel RemoveListenLaterCommandParameter
-        {
-            get { return (ShowViewModel)GetValue(RemoveListenLaterCommandParameterProperty); }
-            set { SetValue(RemoveListenLaterCommandParameterProperty, value); }
-        }
-
-        public EpisodeItemView()
-        {
-            InitializeComponent();
-        }
+    public EpisodeItemView()
+    {
+        InitializeComponent();
     }
 }
