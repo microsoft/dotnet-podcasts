7a9846c60b4bfb53af48e19ea6e9eddf59d77856
James Montemagno <james.montemagno@gmail.com>
2022-09-01 13:56:40 -0700

    Fixes #150

--- src/Mobile/Helpers/Settings.cs
+++ src/Mobile/Helpers/Settings.cs
@@ -4,7 +4,13 @@ public static class Settings
 {
     public static AppTheme Theme
     {
-        get => Enum.Parse<AppTheme>(Preferences.Get(nameof(Theme), Enum.GetName(AppTheme.Light)));
+        get
+        {
+            if (!Preferences.ContainsKey(nameof(Theme)))
+                return AppTheme.Light;
+
+            return Enum.Parse<AppTheme>(Preferences.Get(nameof(Theme), Enum.GetName(AppTheme.Light)));
+        }
         set => Preferences.Set(nameof(Theme), value.ToString());
     }
     
