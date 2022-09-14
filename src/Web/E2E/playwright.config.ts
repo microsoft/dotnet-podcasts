7c16303c9ede3de6c7146b2c63af2e8a0ec63a91
Marcus Felling <marcus.felling@microsoft.com>
2022-09-14 11:26:12 -0500

    use more ci agents

--- src/Web/E2E/playwright.config.ts
+++ src/Web/E2E/playwright.config.ts
@@ -27,8 +27,6 @@ const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html'],
