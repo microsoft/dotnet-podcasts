078fd0eaaf443f4a9a23e4653f30e879179209dd
Marcus Felling <marcus.felling@gmail.com>
2022-09-16 11:18:25 -0500

    add playwright e2e tests

---
+++ src/Web/E2E/tests/login.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect, Page } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('');
+});
+
+test.describe('Login', () => {
+  test('should allow me to login', async ({ page }) => {
+    // click sign in
+    await page.locator('text=Sign In').click();
+    // assert discover page is shown
+    await expect(page).toHaveURL('/discover');
+    expect(page).toHaveTitle('.NET Podcasts')
+  });
+});
---
+++ src/Web/E2E/tests/settings.spec.ts
@@ -0,0 +1,16 @@
+import { test, expect, Page } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/settings');
+});
+
+test.describe('Settings', () => {
+  test('should allow me to toggle settings', async ({ page }) => {
+    // loop through each setting
+    for (const setting of ['autodownload', 'deleteplayed', 'systemtheme', 'darktheme']) {
+      // toggle setting
+      await page.locator('input[name="' + setting + '"]').check();
+      await page.locator('input[name="' + setting + '"]').uncheck();
+    }
+  });
+});
