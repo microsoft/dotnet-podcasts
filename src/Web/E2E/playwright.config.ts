196c3973efdc5beaa6986bdfb8b03c2d83507dd7
Marcus Felling <marcus.felling@gmail.com>
2022-09-23 14:14:13 -0500

    3 retries for flaky tests

--- src/Web/E2E/playwright.config.ts
+++ src/Web/E2E/playwright.config.ts
@@ -17,7 +17,7 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 3 : 0,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['list'],
