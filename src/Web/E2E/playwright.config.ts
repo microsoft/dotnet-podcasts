2b0d17599db5e8ceab436b65aa1bac74b6ec68b9
Marcus Felling <marcus.felling@gmail.com>
2022-10-04 15:17:40 -0500

    set baseURL via env var

--- src/Web/E2E/playwright.config.ts
+++ src/Web/E2E/playwright.config.ts
@@ -29,8 +29,7 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    //baseURL: process.env.BASEURL,
-    baseURL: 'https://dotnetpodcasts.azurewebsites.net',
+    baseURL: process.env.BASEURL,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on',
     video: 'on',
