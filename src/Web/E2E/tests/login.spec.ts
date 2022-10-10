963a332e0b7701bcbd264af538292e21b8f2d194
Marcus Felling <marcus.felling@gmail.com>
2022-10-10 18:13:50 -0500

    add aad login test

--- src/Web/E2E/tests/login.spec.ts
+++ src/Web/E2E/tests/login.spec.ts
@@ -2,11 +2,21 @@ import { test, expect, Page } from '@playwright/test';
 
 test.describe('Login', () => {
   test('should allow me to login', async ({ page }) => {
-    await page.goto('');
-    // click sign in
-    await page.locator('text=Sign In').click();
-    // assert discover page is shown
-    await expect(page).toHaveURL('/discover');
-    expect(page).toHaveTitle('.NET Podcasts')
+    await page.goto('/discover');
+    // Fill username via env variable
+    await page.locator('[placeholder="Email\\, phone\\, or Skype"]').fill(process.env.AADUSERNAME);
+    // Press Enter
+    await page.locator('[placeholder="Email\\, phone\\, or Skype"]').press('Enter');
+    // // Fill password via env variable
+    await page.locator('[placeholder="Password"]').fill(process.env.AADPASSWORD);
+    // Click Sign In
+    await page.locator('input:has-text("Sign in")').click();
+    // Click text=Yes
+    await page.locator('text=Yes').click();
+    // If new AAD permissions are presented, click Yes
+    const terms = await page.$('text=Yes');
+    if (terms) {
+    await terms.click();
+    }
   });
 });
