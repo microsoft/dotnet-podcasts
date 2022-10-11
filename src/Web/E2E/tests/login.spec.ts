cccbcfb0131002c2c6f684f9ba80509165f13731
Marcus Felling <marcus.felling@gmail.com>
2022-10-10 19:01:55 -0500

    login

--- src/Web/E2E/tests/login.spec.ts
+++ src/Web/E2E/tests/login.spec.ts
@@ -4,22 +4,17 @@ test.describe('Login', () => {
   test('should allow me to login', async ({ page }) => {
     await page.goto('/discover');
     // Fill username via env variable
-    await page.locator('[placeholder="Email\\, phone\\, or Skype"]').fill('marcus.felling@gmail.com');
+    await page.locator('[placeholder="Email\\, phone\\, or Skype"]').fill(process.env.AADUSERNAME);
     // Press Enter
     await page.locator('[placeholder="Email\\, phone\\, or Skype"]').press('Enter');
     // // Fill password via env variable
-    await page.locator('[placeholder="Password"]').fill('MSFTagrosk8!');
+    await page.locator('[placeholder="Password"]').fill(process.env.AADPASSWORD);
     // Click Sign In
     await page.locator('input:has-text("Sign in")').click();
-    // Click text=Yes
-    await page.locator('text=Yes').click();
-    // If new AAD permissions are presented, click Yes
-    const terms = await page.$('text=Yes');
-    if (terms) {
-    await terms.click();
-    }
+    // Click No to not remember login
+    await page.locator('text=No').click();
     // assert discover page is shown
     await expect(page).toHaveURL('/discover');
-    expect(page).toHaveTitle('.NET Podcasts')
+    await expect(page).toHaveTitle('.NET Podcasts');
   });
 });
