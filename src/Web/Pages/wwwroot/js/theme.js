03710de7f8c39e9c9ea00b3d766387853ab617cd
trba1810 <trba8l@live.com>
2023-08-25 03:01:39 +0200

    fixed issue with theme

--- src/Web/Pages/wwwroot/js/theme.js
+++ src/Web/Pages/wwwroot/js/theme.js
@@ -1,6 +1,14 @@
-﻿const theme = localStorage.getItem('theme')
-    ?? matchMedia('(prefers-color-scheme: dark)').matches
-        ? 'Dark' : 'Light';
-if (theme === 'Dark') {
-    document.body.setAttribute('data-theme', theme);
+﻿const choice = localStorage.getItem('theme');
+if (choice != null) {
+    document.body.setAttribute('data-theme', choice)
 }
+else {
+    const theme = localStorage.getItem('theme')
+        ?? matchMedia('(prefers-color-scheme: dark)').matches
+        ? 'Dark' : 'Light';
+    if (theme === 'Dark') {
+        document.body.setAttribute('data-theme', theme);
+    }
+}
+
+
